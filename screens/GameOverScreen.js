337875f07e7d029d15ef83e8088d25bb29f9b934
AtallahFatma <fatma.atallah@gmail.com>
2021-12-02 15:41:30 +0100

    insert & styling images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
+import colors from '../config/colors';
 
 function GameOverScreen({ guessRounds, userNumber, restartGame }) {
     return (
         <View style={styles.screen}>
             <TitleText>Gongratulations ! you won ..</TitleText>
-            <BodyText>Number of rounds: {guessRounds}</BodyText>
-            <BodyText>Number was: {userNumber}</BodyText>
+            <View style={styles.imageContainer}>
+                <Image
+                    fadeDuration={100}
+                    style={styles.image}
+                    resizeMode="cover"
+                    //source={require('../assets/success.png')} 
+                    source={{ uri: "https://images.assetsdelivery.com/compings_v2/nastyaaroma/nastyaaroma1604/nastyaaroma160400051.jpg" }}
+                />
+            </View>
+            <View style={styles.resultContainer}>
+                <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{guessRounds} </Text>
+                    rounds to guess the number <Text style={styles.highlight}>{userNumber}</Text></BodyText>
+            </View>
             <Button title="Restart" onPress={restartGame} />
         </View>
     );
@@ -18,6 +30,32 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: colors.black,
+        width: '80%',
+        height: 300,
+        overflow: 'hidden',
+        marginVertical: 20
+    },
+    image: {
+        height: '100%',
+        width: '100%'
+    },
+    highlight: {
+        color: colors.indianPink,
+        fontFamily: 'OpenSansBold'
+    },
+    resultContainer: {
+        width: '80%',
+        marginHorizontal: 20
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 18,
+        marginVertical: 10
     }
 })
 export default GameOverScreen;
