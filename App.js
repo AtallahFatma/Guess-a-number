67a31803f8a1f2fabcbb4931c5f9f5fd6606b6c8
AtallahFatma <fatma.atallah@gmail.com>
2021-12-09 11:10:37 +0100

    fix font loading and add loader

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
 import colors from './config/colors';
 
 import GameScreen from './screens/GameScreen';
@@ -9,22 +8,23 @@ import GameOverScreen from './screens/GameOverScreen';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 
-const fetchFonts = () => {
-  Font.loadAsync({
-    'OpenSansBold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
-  })
-}
-
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!dataLoaded) {
-    return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} onError={console.warn} />
+  const fetchFonts = async () => {
+    await Font.loadAsync({
+      'OpenSansBold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    })
+    setFontLoaded(true);
   }
+
+  useEffect(() => {
+    fetchFonts();
+  })
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
@@ -48,16 +48,26 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header title={guessRounds > 0 ? "The Game is over!" : "Guess a number"} />
-      {content}
-    </View>
+    !fontLoaded ?
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" color={colors.indianPink} />
+      </View> :
+      <View style={styles.container}>
+        <Header title={guessRounds > 0 ? "The Game is over!" : "Guess a number"} />
+        {content}
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.white
+    backgroundColor: colors.white,
+    justifyContent: 'center'
   },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
 });
